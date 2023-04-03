39589c19428c5042aabf60d6a6bf5b9b7fb27fa9
myxans <mushenko1992@gmail.com>
2023-04-03 16:58:13 +0300

    add_remote2

--- index.js
+++ index.js
@@ -1,44 +1,84 @@
-/*const requestURL = 'https://jsonplaceholder.typicode.com/posts';
-
-function sendRequest(method, url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-
-        xhr.open(method, url);
-
-        xhr.responseType = 'json';
+const inputNumberPost = document.querySelector('.search__input');
+const buttonInput = document.querySelector('.search__button');
+const postBlock = document.querySelector('.post_block');
+const commentsBlock = document.querySelector('.comments_block');
+const baseUrl = 'https://jsonplaceholder.typicode.com';
 
 
-        xhr.onload = () => {
-            resolve(xhr.response)
-        };
-        xhr.onerror = () => {
-            reject(xhr.response)
-        }
-
-        xhr.send();
-    })
-};
-
-sendRequest('GET', requestURL)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))*/
-
-const requestURL = 'https://jsonplaceholder.typicode.com/posts';
-
-function sendRequest(url) {
-    return fetch(url).then(response => {
-        return response.json()
+function buttonInputClickHandler() {
+    const id = inputNumberPost.value;
+    if (id && id <= 100 && id > 0) {
+        getData(urlPost())
+            .then(data => renderPostBlock(data));
+    } else {
+        inputNumberPost.value = '';
+        inputNumberPost.placeholder = 'Введіть від 1 до 100';
     }
-    )
-};
-
-function TFun() {
-    let input = document.getElementById('name').value;
-    console.log(input);
-    return input
 }
 
-sendRequest(requestURL)
-    .then(data => console.log(data[TFun()]))
-    .catch(err => console.log(err))
+function urlPost() {
+    const urlPostRes = new URL(`/posts/${inputNumberPost.value}`, baseUrl);
+    return urlPostRes;
+}
+
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Код помилки: ' + response.status);
+                }
+                return response;
+            })
+            .then(response => resolve(response.json()))
+            .catch(error => {
+                rerenderGetError(error);
+                console.log(error);
+            })
+    })
+}
+
+function rerenderGetError (error){
+    postBlock.innerHTML = '';
+    commentsBlock.innerHTML ='';
+    postBlock.insertAdjacentHTML('beforeend',`<p> Вибачте виникла помилка ${error} </p>`);
+}
+
+function renderPostBlock(post) {
+    const title = document.createElement('h1');
+    const content = document.createElement('p');
+    const buttonComments = document.createElement('button');
+    postBlock.innerHTML = '';
+    commentsBlock.innerHTML = '';
+    title.innerText = post.title;
+    title.classList.add('post_block__title');
+    content.innerHTML = post.body;
+    content.classList.add('post_block__content');
+    buttonComments.innerText = 'Коментарі';
+    buttonComments.classList.add('post_block__button');
+    buttonComments.dataset.id = post.id;
+    postBlock.prepend(content);
+    postBlock.prepend(title);
+    postBlock.append(buttonComments);
+    buttonComments.addEventListener('click', buttonCommentsClickHandler);
+}
+
+function buttonCommentsClickHandler(e) {
+    const urlComments = new URL(`/comments`, baseUrl);
+    const id = e.target.dataset.id;
+    urlComments.searchParams.set('id', id);
+    getData(urlComments)
+        .then((data) => {renderComments(data)})
+}
+
+function renderComments(arr) {
+    for (const el of arr) {
+        let res = '';
+        res += `<h2>${el.name}</h2>
+        <p>${el.body}</p>
+        <p>${el.email}</p>`;
+        commentsBlock.insertAdjacentHTML('beforeend', res);
+    }
+}
+
+buttonInput.addEventListener('click', buttonInputClickHandler);
